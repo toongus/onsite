ddb3fcdea5a8e30638ac9375560ce8be1f409005
toongus <toongus@hotmail.com>
2016-07-01 12:34:51 +0700

    update

--- TestMember/wp-content/themes/pingraphy/header.php
+++ TestMember/wp-content/themes/pingraphy/header.php
@@ -6,7 +6,6 @@
  *
  * @package Pingraphy
  */
-
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
--- TestMember/wp-content/themes/pingraphy/index.php
+++ TestMember/wp-content/themes/pingraphy/index.php
@@ -1,18 +1,4 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package Pingraphy
- */
-
-get_header(); ?>
-
+<?php get_header(); ?>
 	<div id="primary" class="content-area content-masonry">
 		<main id="main" class="site-main" role="main">
 			<div id="masonry-container">
@@ -30,10 +16,7 @@ get_header(); ?>
 							 * If you want to override this in a child theme, then include a file
 							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 							 */
-
-							
 								get_template_part( 'template-parts/content', get_post_format());
-						
 
 						?>
 
