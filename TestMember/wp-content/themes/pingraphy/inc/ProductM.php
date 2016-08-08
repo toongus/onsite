4b4d1b7fdd80e33e0c364465f41ca4a7e89dccc7
toongus <toongus@hotmail.com>
2016-08-08 22:14:04 +0700

    Update 002

---
+++ TestMember/wp-content/themes/pingraphy/inc/ProductM.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author teerayuthkaewta
+ *
+ */
+class ProductM
+{
+	private $keyId;
+	private $createBy;
+	private $createDate;
+	private $updateBy;
+	private $updateDate;
+	private $startDate;
+	private $expiredDate;
+	private $status;//
+	private $level;//
+}
